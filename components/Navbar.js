dfedb488990a4c092afdb0eec5fb6af12bbe12e2
andy19910102 <andy19910102@gmail.com>
2024-06-22 08:47:18 +0800

    feat: change navbar text color

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,17 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav className="shadow-sm w-full h-[50px] flex justify-between items-center px-5 fixed z-[999] top-0 bg-white">
-            <Link href="/" className="font-bold text-lg hover:text-pink-500">
+            <Link href="/" className="font-bold text-lg text-slate-900 hover:text-pink-500">
                 NextJS with OpenAI
             </Link>
             <div>
-                <Link href="/" className="mr-3 hover:text-pink-500">
+                <Link href="/" className="mr-3 text-slate-900 hover:text-pink-500 font-semibold">
                     AI VocabularyGen
                 </Link>
-                <Link href="/image-generator" className="mr-3 hover:text-pink-500">
+                <Link href="/image-generator" className="mr-3 text-slate-900 hover:text-pink-500 font-semibold">
                     AI ImgGen
                 </Link>
-                <Link href="/vision" className="hover:text-pink-500">
+                <Link href="/vision" className="text-slate-900 hover:text-pink-500 font-semibold">
                     AI Vision
                 </Link>
             </div>
