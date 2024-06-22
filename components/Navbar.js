022c8ede19f8b774ae3ec2397f697fca5052b154
andy19910102 <andy19910102@gmail.com>
2024-06-22 23:06:32 +0800

    feat: mobile layout for navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
+    const pathname = usePathname()
+    const [showMenu, setShowMenu] = useState(false);
+
+    useEffect(() => {
+        // 當所在路徑頁面改變時，關閉選單
+        setShowMenu(false);
+    }, [pathname])
+
     return (
-        <nav className="shadow-sm w-full h-[50px] flex justify-between items-center px-5 fixed z-[999] top-0 bg-white">
+        <nav className={`navbar ${showMenu ? "show-menu" : ""} shadow-sm w-full h-[50px] flex justify-between items-center px-5 fixed z-[999] top-0 bg-white`}>
             <Link href="/" className="font-bold text-lg text-slate-900 hover:text-pink-500">
                 NextJS with OpenAI
             </Link>
-            <div>
+            <div className="navbar-menu">
                 <Link href="/" className="mr-3 text-slate-900 hover:text-pink-500 font-semibold">
                     AI VocabularyGen
                 </Link>
@@ -17,6 +28,12 @@ export default function Navbar() {
                     AI Vision
                 </Link>
             </div>
+            <button
+                className="menu-btn"
+                onClick={() => setShowMenu(!showMenu)}
+            >
+                <span></span>
+            </button>
         </nav>
     )
 }
