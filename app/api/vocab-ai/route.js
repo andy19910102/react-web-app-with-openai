56d7486588feb74309f29c8170a1bb2cb0b6e66c
andy19910102 <andy19910102@gmail.com>
2024-07-27 20:21:30 +0800

    feat: set default model as gpt-4o-mini

--- app/api/vocab-ai/route.js
+++ app/api/vocab-ai/route.js
@@ -3,7 +3,7 @@ import openai from "@/services/openai";
 export async function POST(req) {
     const body = await req.json();
     console.log("body:", body);
-    // TODO: 透過GPT-4o模型讓AI回傳相關單字
+    // TODO: 透過gpt-4o-mini模型讓AI回傳相關單字
     // 文件連結：https://platform.openai.com/docs/guides/text-generation/chat-completions-api?lang=node.js
     // JSON Mode: https://platform.openai.com/docs/guides/text-generation/json-mode?lang=node.js
     const systemPrompt = `請作為一個單字聯想AI根據所提供的單字聯想5個相關單字`;
@@ -14,10 +14,11 @@ export async function POST(req) {
             { "role": "system", "content": systemPrompt },
             { "role": "user", "content": propmpt }
         ],
-        model: "gpt-4o",
+        model: "gpt-4o-mini",
     };
     // const completion = await openai.chat.completions.create(openAIReqBody);
-    // console.log("completion:", completion);
+    // const payload = completion.choices[0].message.content;
+    // console.log("payload:", payload);
 
     return Response.json({ message: "Success" });
 }
